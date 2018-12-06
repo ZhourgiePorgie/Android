3e3d0414f80037ce8204a7a907f9e89abef1eaa6
Han Zhou <han_zhou@berkeley.edu>
2018-12-06 03:09:44 -0800

    fixed push things

--- BitCoinConverter/app/src/main/java/com/example/android/bitcoinconverter/MainActivity.java
+++ BitCoinConverter/app/src/main/java/com/example/android/bitcoinconverter/MainActivity.java
@@ -4,22 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.PersistentCookieStore;
-
-import java.net.CookieManager;
-import java.net.CookiePolicy;
-import java.net.CookieStore;
-import java.net.HttpCookie;
-import java.util.Date;
-
-import cz.msebera.android.httpclient.cookie.Cookie;
-import cz.msebera.android.httpclient.impl.cookie.BasicClientCookie;
 
 public class MainActivity extends AppCompatActivity {
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Android
+Some Android projects I've created.
+
+# HappyBirthday
+A static birthday card app that wishes someone a Happy Birthday!
+
+# JustJava
+Takes in a name, a selection of toppings by using checkboxes, and a quantity to produce a coffee order summary.  This order summary can then be emailed to a desired email.
+
+# Bitcoin Converter
+Takes in a name and remembers the name for future uses.  Asks for a currency and shows the amount 1 bitcoin is worth in that currency.
