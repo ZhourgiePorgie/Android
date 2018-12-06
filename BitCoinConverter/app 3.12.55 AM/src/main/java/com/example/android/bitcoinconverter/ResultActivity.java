96a4e2eaeef7a8c7b45d4bd49de1da5742f79237
Han Zhou <han_zhou@berkeley.edu>
2018-12-06 03:13:40 -0800

    restored files

---
+++ BitCoinConverter/app 3.12.55 AM/src/main/java/com/example/android/bitcoinconverter/ResultActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.bitcoinconverter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private String currency;
+    private TextView message_view;
+    private String message;
+    private float rate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        //Receives the currency and rate and updates the message
+        Intent intent = getIntent();
+        currency = intent.getExtras().get("currency").toString();
+        rate = intent.getFloatExtra("rate", 0f);
+        message_view = (TextView) findViewById(R.id.message);
+        message = "One bitcoin is worth " + rate +  " " + currency;
+        message_view.setText(message);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
